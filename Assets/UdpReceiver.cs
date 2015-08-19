043e8c52b05a0d2cabf5e3b556c8792f34f96e3c
Yuji YASUHARA <yuji.yasuhara@gmail.com>
2015-08-19 14:48:38 +0900

    it works.

---
+++ Assets/UdpReceiver.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UdpReceiver : MonoBehaviour {
+	public int listenPort = 2002; // ポートはサーバ・クライアントで合わせる必要がある
+	private static bool received = false;
+
+	private struct UdpState {
+		public System.Net.IPEndPoint e;
+		public System.Net.Sockets.UdpClient u;
+	}
+
+	public static void ReceiveCallback(System.IAsyncResult ar) {
+		System.Net.Sockets.UdpClient u = (System.Net.Sockets.UdpClient)((UdpState)(ar.AsyncState)).u;
+		System.Net.IPEndPoint e = (System.Net.IPEndPoint)((UdpState)(ar.AsyncState)).e;
+		var receiveBytes = u.EndReceive(ar, ref e);
+		var receiveString = System.Text.Encoding.ASCII.GetString(receiveBytes);
+
+		Debug.Log(string.Format("Received: {0}", receiveString)); // ここに任意の処理を書く
+
+		received = true;
+	}
+
+	IEnumerator receive_loop() {
+		var e = new System.Net.IPEndPoint(System.Net.IPAddress.Any, listenPort);
+		var u = new System.Net.Sockets.UdpClient(e);
+		var s = new UdpState();
+		s.e = e;
+		s.u = u;
+		for (;;) {
+			received = false;
+			u.BeginReceive(new System.AsyncCallback(ReceiveCallback), s);
+			while (!received) {
+				yield return null;
+			}
+		}
+	}
+
+	void Start () {
+		StartCoroutine(receive_loop());
+	}
+}
---
+++ Assets/UdpSender.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UdpSender : MonoBehaviour {
+	public int listenPort = 2002; // ポートはサーバ・クライアントで合わせる必要がある
+	private static bool sent = false;
+	private Coroutine coroutine_ = null;
+
+	public static void SendCallback(System.IAsyncResult ar)
+	{
+		// System.Net.Sockets.UdpClient u = (System.Net.Sockets.UdpClient)ar.AsyncState;
+		sent = true;
+	}
+
+	IEnumerator send(string server, string message)
+	{
+		Debug.Log("sending..");
+		var u = new System.Net.Sockets.UdpClient();
+		u.Connect(server, listenPort);
+		var sendBytes = System.Text.Encoding.ASCII.GetBytes(message);
+		sent = false;
+		u.BeginSend(sendBytes, sendBytes.Length, 
+					new System.AsyncCallback(SendCallback), u);
+		while (!sent) {
+			yield return null;
+		}
+		u.Close();
+		coroutine_ = null;
+		Debug.Log("done.");
+	}
+
+	void Update() {
+		if (coroutine_ == null && Input.GetKeyDown(KeyCode.Space)) {
+            string sendMsg = "space key!";	 // 任意のテキスト（長すぎないこと）
+			string server = "127.0.0.1"; // ブロードキャストアドレスが使えるならそれがベスト。できなかったら相手のＩＰアドレスを直打ち
+			coroutine_ = StartCoroutine(send(server, sendMsg));
+        }
+    }
+}
